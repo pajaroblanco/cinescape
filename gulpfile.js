4e3208c99c34fa31de6505cb9a43ee93befd6ada
Brandon Ayers <brandona@ucr.edu>
2016-06-02 07:09:40 -0700

    Started setting up gulpfile js/css dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,55 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
 var concat = require("gulp-concat");
+var $    = require('gulp-load-plugins')();
 
-gulp.task("js.es6", function () {
-    return gulp.src(["app/registerComponent.js", "app/**/*.js", '!app/bower_components/**/*.js'])
+var sassPaths = [
+    'app/bower_components/foundation-sites/scss',
+    'app/bower_components/motion-ui/src',
+    'app/scss/**/*.scss'
+];
+
+var jsPaths = [
+    "app/bower_components/jquery/dist/jquery.js",
+    "app/bower_components/velocity/velocity.js",
+    "app/bower_components/velocity/velocity.ui.js",
+    "app/bower_components/angular/angular.js",
+    "app/bower_components/angular-animate/angular-animate.js",
+    "app/bower_components/angular-route/angular-route.js",
+    "app/bower_components/angular-velocity-revival/angular-velocity.js",
+    "app/bower_components/angular/angular.js",
+    "app/registerComponent.js",
+    "app/angular-app/**/*.js"
+];
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/app.scss')
+        .pipe($.sass({
+            includePaths: sassPaths
+        })
+            .on('error', $.sass.logError))
+        .pipe($.autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9']
+        }))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task("js", function () {
+    return gulp.src(jsPaths)
         //.pipe(sourcemaps.init())
         .pipe(babel())
-        .pipe(concat("all.js"))
+        .pipe(concat("app.js"))
         //.pipe(sourcemaps.write("."))
         .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task("html", function() {
+    return gulp.src("app/*.html")
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task('default', ['sass', 'js', 'html'], function() {
+    gulp.watch(['app/scss/**/*.scss'], ['sass']);
+    gulp.watch(['app/angular-app/**/*.js'], ['js']);
+    gulp.watch(['app/*.html'], ['html']);
 });
